ca617e64c4ad9b6c9c2f3c288c8c7407106683aa
santosh1505 <chkumar6565@gmail.com>
2025-07-07 21:35:22 +0530

    Updated

---
+++ demoExpress/routes/demo.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+router.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, '../templates/index.html'))
+})
+
+module.exports=router
