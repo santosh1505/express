2214329e250cee1419fc685dbbd867d54299caab
santosh1505 <chkumar6565@gmail.com>
2025-07-07 23:21:28 +0530

    Updated

---
+++ demoExpress/data/blog.js
@@ -0,0 +1,20 @@
+blog = [
+ {
+    title:"This is ExpressJS",
+    content: "This is ExpressJS content",
+    slug: "ExpressJS-learn"
+ },
+
+{
+    title:"This is DJango",
+    content: "This is Django content",
+    slug : "Django-learn"
+ },
+ {
+    title: "This is Python",
+    content: "This is Python content",
+    slug: "Python-learn" 
+  }
+]
+
+module.exports = blog;
--- demoExpress/routes/demo.js
+++ demoExpress/routes/demo.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const blog = require('../data/blog')
 const router = express.Router()
 
 
@@ -9,6 +10,21 @@ router.get('/', (req, res) => {
 
 router.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, '../templates/index.html'))
+})
+
+router.get('/blog', (req, res) => {
+  blog.forEach(e => {
+    console.log(e.title)
+  });
+  res.sendFile(path.join(__dirname, '../templates/blog.html'))
+})
+
+router.get('/blog/:slug', (req, res) => {
+  myblog = blog.find((e)=>{
+  return e.slug==req.params.slug
+  })
+  console.log(myblog)
+  res.sendFile(path.join(__dirname, '../templates/blogs.html'))
 })
 
 module.exports=router
